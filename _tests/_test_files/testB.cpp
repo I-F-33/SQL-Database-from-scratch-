f5fd9efe87537171ec8a308c385ec25e4b9c5ced
Flores33 <ivan_flores33@yahoo.com>
2023-12-14 15:02:03 -0800

    working

--- _tests/_test_files/testB.cpp
+++ _tests/_test_files/testB.cpp
@@ -24,17 +24,17 @@ using namespace std;
         "insert into players values James, Harden, 32, Guard, \"Brooklyn Nets\"",
         "insert into players values Anthony, Davis, 28, Forward, \"Los Angeles Lakers\"",
         "insert into players values Kawhi, Leonard, 30, Forward, \"Los Angeles Clippers\"",
-        "insert into players values Joel, Embiid, 27, Center, \"Philadelphia 76ers\""
-        // "insert into players values Devin, Booker, 25, Guard, \"Phoenix Suns\"",
-        // "insert into players values Jayson, Tatum, 23, Forward, \"Boston Celtics\"",
-        // "insert into players values \"Karl Anthony\", Towns, 26, Center, \"Minnesota Timberwolves\"",
-        // "insert into players values Trae, Young, 23, Guard, \"Atlanta Hawks\"",
-        // "insert into players values Jimmy, Butler, 32, Forward, \"Miami Heat\"",
-        // "insert into players values Donovan, Mitchell, 25, Guard, \"Utah Jazz\"",
-        // "insert into players values Bam, Adebayo, 24, Center, \"Miami Heat\"",
-        // "insert into players values Rudy, Gobert, 29, Center, \"Utah Jazz\"",
-        // "insert into players values Chris, Paul, 37, Guard, \"Phoenix Suns\"",
-        // "insert into players values Russell, Westbrook, 33, Guard, \"Los Angeles Lakers\""
+        "insert into players values Joel, Embiid, 27, Center, \"Philadelphia 76ers\"",
+        "insert into players values Devin, Booker, 25, Guard, \"Phoenix Suns\"",
+        "insert into players values Jayson, Tatum, 23, Forward, \"Boston Celtics\"",
+        "insert into players values \"Karl Anthony\", Towns, 26, Center, \"Minnesota Timberwolves\"",
+        "insert into players values Trae, Young, 23, Guard, \"Atlanta Hawks\"",
+        "insert into players values Jimmy, Butler, 32, Forward, \"Miami Heat\"",
+        "insert into players values Donovan, Mitchell, 25, Guard, \"Utah Jazz\"",
+        "insert into players values Bam, Adebayo, 24, Center, \"Miami Heat\"",
+        "insert into players values Rudy, Gobert, 29, Center, \"Utah Jazz\"",
+        "insert into players values Chris, Paul, 37, Guard, \"Phoenix Suns\"",
+        "insert into players values Russell, Westbrook, 33, Guard, \"Los Angeles Lakers\""
     };
 
     for(int i = 0; i < commands.size(); i++)
@@ -53,10 +53,33 @@ using namespace std;
 
     cout << sql.command("select * from players where team <= \"Los Angeles Lakers\"") ;
 
+  cout << "testing >= and < or" << endl;
+  cout << sql.command("select * from players where (age >= \"28\" and team = \"Los Angeles Lakers\") and (team = \"Los Angeles Lakers\") or team = \"Utah Jazz\"") << endl;
+
+cout << "select where x or y and z" << endl;
+
+cout << sql.command("select * from players where team = \"Los Angeles Lakers\" or team = \"Utah Jazz\" and age = \"29\"") << endl;
+
+cout << "select where x and y or z and a" << endl;
+
+cout << sql.command("select * from players where team = \"Los Angeles Lakers\" and team = \"Utah Jazz\" or age = \"29\" and age = \"30\"") << endl;
+
+cout << "select where x or y and z or a" << endl;
+
+cout << sql.command("select * from players where (team = \"Los Angeles Lakers\" or age > 25) and age < 30 or age = \"30\"") << endl;
+
+cout << "select where x and y or z or a" << endl;
+
+cout << sql.command("select * from players where team = \"Los Angeles Lakers\" and age > 25 or age < 30 or age = \"30\"") << endl;
+
+cout << "select where x or y or z and a" << endl;
+
+cout << sql.command("select * from players where team = \"Los Angeles Lakers\" or age < 30 or age = \"30\" and team = \"Utah Jazz\"") << endl;
 
 
+cout << "select where (x and y) or (p or q) and z" << endl;
 
-
+cout << sql.command("select * from players where (team = \"Los Angeles Lakers\" and age < 30) or (age = \"30\" or age = \"29\") and team = \"Utah Jazz\"") << endl;
 
   }
   
